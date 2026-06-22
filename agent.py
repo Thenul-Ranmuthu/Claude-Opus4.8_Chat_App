62b901f33c70f28a36ab7c03e14e8c815daa7d3b
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-06-23 00:49:24 +0530

    Add agent.py to implement AI project client interaction

---
+++ agent.py
@@ -0,0 +1,25 @@
+# Before running the sample:
+#    pip install azure-ai-projects>=2.1.0
+
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+
+endpoint = "https://thenul-azureai-test1-resource.services.ai.azure.com/api/projects/thenul-azureai-test1"
+
+project_client = AIProjectClient(
+    endpoint=endpoint,
+    credential=DefaultAzureCredential(),
+)
+
+my_agent = "Chat-Agent"
+my_version = "1"
+
+openai_client = project_client.get_openai_client()
+
+# Reference the agent to get a response
+response = openai_client.responses.create(
+    input=[{"role": "user", "content": "Tell me what you can help with."}],
+    extra_body={"agent_reference": {"name": my_agent, "version": my_version, "type": "agent_reference"}},
+)
+
+print(f"Response output: {response.output_text}")
